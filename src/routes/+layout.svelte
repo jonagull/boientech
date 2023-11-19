1e46949729a2ecb44069a658836d76c838145c10
jonagull <jonathangullik@gmail.com>
2023-11-19 14:00:20 +0100

    css setup, font setup, langinpage work

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "../fonts.css";
+    import "../global.css";
+</script>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: "Akira";
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,25 @@
-<script>
-    let ctaText = "Get Started";
-    let backgroundImagePath = "your-background-image.jpg"; // Replace with the actual path to your image
-</script>
-
-<nav>
-    <!-- You can add navigation links here -->
-    <a href="/">Home</a>
-    <a href="/blog">blog</a>
-    <a href="#">About</a>
-    <a href="#">Contact</a>
-</nav>
-
-<div class="cta-container">
-    <h1>Your Landing Page</h1>
-    <p>Awesome description about your product or service.</p>
-    <a href="#" class="cta-button">{ctaText}</a>
+<div class="hero-wrapper">
+    <div class="hero-container">
+        <h1>boientech</h1>
+    </div>
 </div>
 
 <style>
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: Arial, sans-serif;
-        background-image: url({backgroundImagePath});
-        background-size: cover;
-        background-position: center;
-        height: 100vh;
+    /* .hero-container {
         display: flex;
-        align-items: center;
         justify-content: center;
-        color: #fff;
-    }
+        height: 100vh;
+    } */
 
-    nav {
-        background-color: rgba(0, 0, 0, 0.5);
-        padding: 10px;
-        position: fixed;
-        width: 100%;
-        z-index: 1000;
-    }
+    .hero-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
 
-    nav a {
-        color: #fff;
-        text-decoration: none;
-        margin: 0 15px;
-        font-weight: bold;
-    }
-
-    .cta-container {
-        text-align: center;
-    }
-
-    .cta-button {
-        display: inline-block;
-        padding: 10px 20px;
-        font-size: 18px;
-        font-weight: bold;
-        text-decoration: none;
-        background-color: #4caf50;
-        color: #fff;
-        border-radius: 5px;
-        transition: background-color 0.3s;
-    }
-
-    .cta-button:hover {
-        background-color: #45a049;
+        & h1 {
+            font-size: 70px;
+            letter-spacing: 2px;
+        }
     }
 </style>
